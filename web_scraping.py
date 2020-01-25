8959328c413ff08247e0c4908b5539b06ce1a150
AB3000 <aarushban@gmail.com>
2020-01-25 00:16:56 -0500

    Added Scraping functionality for a single webpage

--- web_scraping.py
+++ web_scraping.py
@@ -1,70 +1,46 @@
-#for reading the webpage
-import pandas as pd
-
-from nltk.stem import SnowballStemmer
-from nltk.tokenize import sent_tokenize
-from nltk.tokenize import word_tokenize
-import urllib.request
+# importing the necessary packages
+import requests
 from bs4 import BeautifulSoup
-
-import regex as re  # regex matching
-
-"""
-Algorithm: 
-Crawled the webpage and extracted all the sentences from it.
-Then went through each attribute, and picked out relevant sentences.
-Then trained the model for each attribute and fed website sentences into it. 
-"""
-import nltk
-# import list of stopwords (conjunctions, prepositions) and dictionary words
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet 
-
-# import sentence splitting algorithm tools
-from gensim.models.doc2vec import Doc2Vec
-from gensim.models.doc2vec import TaggedDocument
+import numpy as np 
 
 
 #test link
-link = 'https://www.foxnews.com/politics/new-ukraine-charges-fly-amid-giuliani-evidence-secret-trump-tape'
-
-#function to download any necessary ML packages
-def download_packages():
-    nltk.download('stopwords')
-    nltk.download('punkt')
-    nltk.download('wordnet')
-    nltk.download('words')
-
-def assess_website(link):
-    """
-        Parameters: 
-            link: the URL to the policy
-        Returns: 
-            clean_tokens: All sentences on the website 
-    """
-    # read the webpage
-    response = urllib.request.urlopen(link)
-    html = response.read()
-
-    # take out sentences/tokens from webapge
-    soup = BeautifulSoup(html, 'html5lib')
-    text = soup.body.get_text(strip=True)
-    sentences = (sent_tokenize(text))
-
-    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
-
-    for words in sentences[:]:
-        # lazy way of removing sentences with just html in webpage
-        if '{' in words or '}' in words or "[]" in words:
-            sentences.remove(words)
-            continue
-
-    return sentences
+link = 'https://en.wikipedia.org/wiki/Donald_Trump'
 
 
-#TO TEST PYTHON CODE SEPARATELY
+def scrape_article(url):
+    res = requests.get(link)
+    if (res.status_code == 200 and 'content-type' in res.headers and
+        res.headers.get('content-type').startswith('text/html')):
+            html = res.text
+   
+    soup = BeautifulSoup(html, 'html.parser')# find the article title
+    
+    h1 = soup.body.find('h1')# find the common parent for <h1> and all <p>s.
+    root = h1
+
+    #an article may not have fewer than 4 paragraphs
+    while root.name != 'body' and len(root.find_all('p')) < 4:
+        root = root.parent
+
+    #getting all meaningful paragraphs/content
+    ps = root.find_all(['h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre'])
+    ps.insert(0, h1)    # add the title
+    content = [tag2md(p) for p in ps]
+    print(content)
+
+def tag2md(tag):
+    if tag.name == 'p':
+        return tag.text
+    elif tag.name == 'h1':
+        return f'{tag.text}\n{"=" * len(tag.text)}'
+    elif tag.name == 'h2':
+        return f'{tag.text}\n{"-" * len(tag.text)}'
+    elif tag.name in ['h3', 'h4', 'h5', 'h6']:
+        return f'{"#" * int(tag.name[1:])} {tag.text}'
+    elif tag.name == 'pre':
+        return f'```\n{tag.text}\n```'
+
 if __name__ == "__main__":
-
-    #UNCOMMENT IF YOU HAVE NOT DOWNLOADED NLTK PACKAGES
-    download_packages()
-    print(assess_website(link))
+    scrape_article(link) 
+    
