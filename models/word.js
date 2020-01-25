805c39524cf5a4baf0fa8eb8a340b35a87f15b77
AB3000 <aarushban@gmail.com>
2020-01-25 14:18:28 -0500

    Word Schema Template

---
+++ models/word.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+//in case we need something to be unique 
+// const uniqueValidator = require('mongoose-unique-validator');
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+const Schema = mongoose.Schema;
+
+let wordSchema = new Schema({
+
+});
