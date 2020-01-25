5e23d3fdd763f8e169c666a2cd5bb47ebf4911df
AB3000 <aarushban@gmail.com>
2020-01-24 23:01:49 -0500

    Create web_scraping.py

---
+++ web_scraping.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+import urllib.request
+from bs4 import BeautifulSoup
+
+
