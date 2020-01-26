cd7306214097f5a32c2bd248defc7f7728a1d6c0
lukecheng1998 <lukecheng1998@gmail.com>
2020-01-25 23:54:44 -0500

    removal of 3000 from port

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ mongoose.set("useFindAndModify", false);
 
 
 const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 console.log(process.env.PORT);
 
 app.listen(port, hostname, () => {
