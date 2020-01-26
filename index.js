40be1d009b30517f4e9eef2c5e4bd977e1f084d2
AB3000 <aarushban@gmail.com>
2020-01-26 01:30:15 -0500

    Removed extra comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 var unirest = require("unirest");
-var wordleReq = unirest("POST", "https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud");
+// var wordleReq = unirest("POST", "https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud");
 // var wordleReq = unirest("POST", "https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud?max_words=1000");
 var express = require("express");
 var app = express();
@@ -112,53 +112,10 @@ app.get("/wordle", (req, res) => {
 	py.stdout.on('end', function(){
 	//console.log('TEXT: ',dataString);
 	dataString+=" "+textRetrieved;
-	wordleReq.query({
-		"max_words": "16000",
-		"font_pathstring": "public/fonts/GoudyBookletter1911-Regular.ttf",
-		"language": "en",
-		"colors": "%5B'%23375E97'%2C '%23FB6542'%2C '%23FFBB00'%2C '%233F681C'%5D",
-		"use_stopwords": "true",
-		"scaling": "1.0",
-		"width": "800",
-		"height": "800",
-		"text": "This is a test. I repeat%2C this is a test. We are only testing the functionality of this api%2C nothing else. End of test."
 	});
-	wordleReq.headers({
-		"x-rapidapi-host": "textvis-word-cloud-v1.p.rapidapi.com",
-		"x-rapidapi-key": "43a21f4f50msh8b405ea8a73566ap12a98ajsn7413018624fc",
-		"x-api-key": "qLn10OtYwY8MILjfYAIH11rx6yVWaGMOUNh0NZQh",
-		"content-type": "application/json",
-		"accept": "application/json"
-	});
-	wordleReq.type("json");
-	wordleReq.send({
-		"text": dataString,
-		"scale": 1.0,
-		"width": 800,
-		"height": 800,
-		"colors": [
-			"#ffffff",
-		],
-		"font_pathstring": "public/fonts/GoudyBookletter1911-Regular.ttf",
-		"use_stopwords": true,
-		"language": "en",
-		"uppercase": false
-	});
-
-	wordleReq.end(function (wordleRes) {
-		if (wordleRes.error) throw new Error(wordleRes.error);
-		wordle = wordleRes.body;
-		//res.render("wordle", {base64: wordle, keyword: search})
-		console.log("Wordle Done");
-		hasLoaded = true;
-		res.render("wordle", {base64: wordle, keyword: search});
-	});
-
-	});
-	// py.stdin.write(JSON.stringify(data));
-	// py.stdin.end();
-	//res.render("wordle", {base64: wordle, keyword: search})
-	//res.write("wordle", {base64: wordle, keyword: search});
+	py.stdin.write(JSON.stringify(data));
+	py.stdin.end();
+	res.write("wordle", {base64: wordle, keyword: search});
 });
 
 //Connection start
@@ -196,47 +153,3 @@ py.stdout.on('end', function(){
 });
 py.stdin.write(JSON.stringify(data));
 py.stdin.end();
-/*
-var scraped = "I've never been much of a mobile gamer, but, forget everything you think you know about mobile games because Raid Shadow Legends is one of the most ambitious RPG projects of 2019 has just been released and will change everything. Just look at the level of detail of these characters! If you use the code in the description you can start with 50,000 silver and join the Special Launch Tournament, and you better hurry because it's getting big fast! You can play for totally free with the link below on your smartphone.";
-req.query({
-	"max_words": "200",
-	"font": "Tahoma",
-	"language": "en",
-	"colors": "%5B'%23375E97'%2C '%23FB6542'%2C '%23FFBB00'%2C '%233F681C'%5D",
-	"use_stopwords": "true",
-	"scaling": "0.5",
-	"width": "800",
-	"height": "800",
-	"text": "This is a test. I repeat%2C this is a test. We are only testing the functionality of this api%2C nothing else. End of test."
-});
-req.headers({
-	"x-rapidapi-host": "textvis-word-cloud-v1.p.rapidapi.com",
-	"x-rapidapi-key": "43a21f4f50msh8b405ea8a73566ap12a98ajsn7413018624fc",
-	"x-api-key": "qLn10OtYwY8MILjfYAIH11rx6yVWaGMOUNh0NZQh",
-	"content-type": "application/json",
-	"accept": "application/json"
-});
-req.type("json");
-req.send({
-	"text": scraped,
-	"scale": 0.5,
-	"width": 800,
-	"height": 800,
-	"colors": [
-		"#375E97",
-		"#FB6542",
-		"#FFBB00",
-		"#3F681C"
-	],
-	"font": "Tahoma",
-	"use_stopwords": true,
-	"language": "en",
-	"uppercase": false
-});
-
-
-	//image.src = res.body;
-	wordle = res.body;
-	//console.log(res.body);
-});
-*/
